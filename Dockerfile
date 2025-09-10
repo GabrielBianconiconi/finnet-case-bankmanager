91d2ef9e83586f228f349f2a6b0afde79faed1f3
GabrielBianconiConi <gabrielbianconiconi@gmail.com>
2025-09-10 15:32:58 -0300

    feat: add migrations for inital database schema

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM php:8.1-apache
 
+ENV APACHE_DOCUMENT_ROOT /var/www/html/src/public
+RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
+RUN sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+
 RUN apt-get update && apt-get install -y \
     git \
     zip \
---
+++ phinx.php
@@ -0,0 +1,23 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '/var/www/html/database/migrations',
+        'seeds' => '/var/www/html/database/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => 'db',
+            'name' => 'bankmanager',
+            'user' => 'user',
+            'pass' => 'password',
+            'port' => '3306',
+            'charset' => 'utf8mb4',
+        ],
+    ],
+    'version_order' => 'creation'
+];
